9ca06648724e223395fb9e4aea97017fb98ef0bd
Stapleru <andrejutinka@mail.ru>
2023-08-23 14:22:37 +0300

    Add calculator basic func

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # calculator
+
+Simple GUI calculator made using HTML/CSS/JS
+
+Live Page: TODO;
---
+++ script.js
@@ -0,0 +1,72 @@
+let displayValue = "";
+
+let display = document.querySelector(".display")
+let buttons = document.querySelector(".buttons").querySelectorAll("*")
+
+buttons.forEach(element => {
+    element.addEventListener("click", populateDisplay)
+});
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(a, b, operator){
+    a = parseInt(a);
+    b = parseInt(b);
+    if(operator == "+")
+        return add(a, b);
+    else if(operator == "-")
+        return subtract(a, b);
+    else if(operator == "*")
+        return multiply(a, b);
+    else if(operator == "/")
+        return divide(a, b);
+
+    //MAYBE DO ERROR IF ELSE
+}
+
+function parseText(string){
+    let a, b, operator;
+    [a, operator, b] = string.split(/([/*-+=])/g);
+    return [a, operator, b];
+}
+
+function populateDisplay(){
+    let a, b, operator;
+    let buttonValue = this.textContent;
+    [a, operator, b] = parseText(displayValue);
+    console.log([a, operator, b])
+    if(this.classList.contains('opBtn')){
+        if(a){
+            if(!b)
+                operator = buttonValue;
+            else{
+                a = operate(a, b, operator)
+                b = null; operator = null;
+            }
+        }
+    } else if(this.classList.contains('eqlBtn') && operator && b){
+        a = operate(a, b, operator)
+        b = null; operator = null;
+    } else if(this.classList.contains('numBtn')){
+        if(!operator)
+            a += buttonValue;
+        else
+            b += buttonValue;
+    }
+    displayValue = [a, operator, b].join("");
+    display.textContent = displayValue;
+}
